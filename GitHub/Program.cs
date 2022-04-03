5988bd8378a98109f5b5913c68f20ee03728e14b
honzi <honzi@DESKTOP-P5H99CU>
2022-04-03 19:19:39 +0200

    Přidejte soubory projektu.

---
+++ GitHub/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cislo1 = Convert.ToInt32(Console.ReadLine());
+            int cislo2 = Convert.ToInt32(Console.ReadLine());
+            int[] pole = { cislo1, cislo2 };
+
+
+            if (cislo1 == cislo2)
+               {
+                Console.WriteLine("Čísla jsou si rovná.");
+                Console.ReadLine();
+               }
+
+            else
+                {
+                int mensi_cislo = pole.Min();
+                int vetsi_cislo = pole.Max();
+                Console.WriteLine(mensi_cislo + ", " + vetsi_cislo);
+                Console.ReadLine();
+                }
+        }
+    }
+}
---
+++ GitHub/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení se řídí přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("GitHub")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GitHub")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
+// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
+// komponenty COM, nastavte atribut ComVisible daného typu na hodnotu true.
+[assembly: ComVisible(false)]
+
+// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
+[assembly: Guid("a7f8afab-01fb-425e-9833-5ea21bc94bc2")]
+
+// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+//
+//      Hlavní verze
+//      Podverze
+//      Číslo sestavení
+//      Revize
+//
+// Můžete zadat všechny hodnoty nebo nastavit výchozí číslo buildu a revize
+// pomocí zástupného znaku * takto:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
